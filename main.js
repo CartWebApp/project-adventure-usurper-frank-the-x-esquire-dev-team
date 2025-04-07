c2effbaad90fe9d97a86785acdada79bbfb984c8
CheeseManIsMe <aiden.beijens24@student.cart.org>
2025-04-07 15:45:19 -0700

    Boo

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
+// Character Data
+var player = {};
+
+
+
+
+
+
+
+
 // Dialog typewriter effect
 var i = 0;
 var txt = 'Hey man, how are you?';
 var speed = 50;
-
 
 
 function typeWriter() {
@@ -10,6 +19,7 @@ function typeWriter() {
         document.getElementById("demo").innerHTML += txt.charAt(i);
         i++;
         setTimeout(typeWriter, speed);
+        document.getElementById("demo").scrollIntoView({behavior: `smooth`, block:`end`,});
     }
 }
 
@@ -29,7 +39,6 @@ const mentor = "url(images/characters/goblin2.png)";
 
 function characterChange(newCharacter) {
     document.getElementsByClassName(`npc`)[0].style.backgroundImage = newCharacter;
-    console.log(`boo`);
 }
 
 
@@ -59,7 +68,23 @@ function off() {
     document.getElementById("overlay").style.display = "none";
 }
 
-// Conversation continuation
+// Conversation Data
+const optionButtonsElement = document.getElementById(`option-buttons`);
+
+// const textNodes = [
+//     {
+//         // intro
+//         id: 1,
+//         text: ``,
+//         options: [
+//             {
+//                 text: `Continue`
+//                 nextText: 1,
+//             },
+//         ]
+//     }
+// ]
+
 let response = `Fortnite is an online video game and game platform developed by Epic Games and released in 2017. It is available in seven distinct game mode versions that otherwise share the same general gameplay and game engine: Fortnite Battle Royale, a battle royale game in which up to 100 players fight to be the last person standing; Fortnite: Save the World, a cooperative hybrid tower defense-shooter and survival game in which up to four players fight off zombie-like creatures and defend objects with traps and fortifications they can build; Fortnite Creative, in which players are given complete freedom to create worlds and battle arenas; Lego Fortnite, an open world game collection divided between survival game Lego Fortnite Odyssey and social game Lego Fortnite Brick Life; Rocket Racing, a racing game; Fortnite Festival, a rhythm game; and Fortnite Ballistic, a tactical first-person shooter currently in early access. All game modes except Save the World are free-to-play.`;
 
 function conversationCont(reply) {
@@ -87,3 +112,5 @@ function move() {
         }
     }
 }
+
+
